31aa29547e5ffea2e467b223efb6488f3f169dc5
heman713kumar <heman713kumar@gmail.com>
2025-08-19 06:43:03 +0100

    Add files via upload

---
+++ frontend/src/main.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Optional: if Tailwind or global styles are used
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
